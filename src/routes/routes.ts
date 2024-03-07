2c04ee7b6cab5d76c55cc000c45eac935f1981ec
Abolfazl Aslani <aslaniabolfazl86@gmail.com>
2024-03-07 23:31:51 +0330

    some bug fixes after merge

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,43 @@
 // auth.route.ts
-
+import AuthController from '../controllers/auth.controller';
+import {userSchema,loginSchema} from '../schemas/user.schema';
 import { FastifyPluginAsync } from 'fastify';
 
 
+interface IBody{
+  name:string,
+  lastname: string,
+  phoneNumber:string,
+  email:string,
+  password:string
+
+}
+
+interface IReply{
+  201:{
+    success:true,
+    message:string,
+    body:object
+  },
+  '4xx':{error : string}
+}
+
+//* Authentication Routes
 const routes: FastifyPluginAsync = async (fastify) => {
+
+    //* Signup endpoint
+    fastify.post<{
+    Body:IBody,
+    Reply:IReply
+    }>('/signup', {
+      schema: userSchema.schema,
+      handler: AuthController.signUp,
+    });
     
-    //* Registred auth routes
-   
+    //* Login endpoint
+    fastify.post('/login', {
+        schema: loginSchema.schema,
+        handler: AuthController.Login,
+      });
   };
-  
   export default routes;
